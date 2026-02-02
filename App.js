02fd828fdfff3e1aec1818d11703ae42ff0fcf39
Yuktha Priya Masupalli <52076713+yukthapriya@users.noreply.github.com>
2026-02-02 14:05:31 -0500

    Fix export statement for App component

--- App.js
+++ App.js
@@ -164,8 +164,6 @@
 //     </div>
 //   );
 // };
-
-// export default App;
 import React, { useEffect, useRef, useState } from 'react';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
